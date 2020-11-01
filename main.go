84f13b286a1a3f5eb43ef82f77889908544f381c
photostorm <eephoto45@gmail.com>
2020-10-31 23:22:19 -0400

    added command line flags for map file and for print mode

--- main.go
+++ main.go
@@ -1,24 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/photostorm/tiled"
 )
 
+var (
+	mapFile = flag.String("f", "testIMap.tmx", "Tile map (.tmx)")
+	mode    = flag.String("m", "basic", "control what information is showed")
+)
+
 func main() {
+	flag.Parse()
+
 	var tileMap *tiled.Map
 	var err error
 
-	tileMap, err = tiled.LoadFromFile("testIMap.tmx")
+	tileMap, err = tiled.LoadFromFile(*mapFile)
 	if err != nil {
 		println(err.Error())
 		return
 	}
 
-	// TODO Layer sorter
-
-	fmt.Printf("Map Height: %d", tileMap.Height)
-	fmt.Printf("Map Width: %d", tileMap.Width)
-	fmt.Printf("Tile Height: %d", tileMap.TileHeight)
-	fmt.Printf("Tile Width: %d", tileMap.TileWidth)
+	switch *mode {
+	case "basic":
+		fmt.Printf("Render Order: %s", tileMap.RenderOrder)
+		fmt.Printf("Map Height: %d", tileMap.Height)
+		fmt.Printf("Map Width: %d", tileMap.Width)
+		fmt.Printf("Tile Height: %d", tileMap.TileHeight)
+		fmt.Printf("Tile Width: %d", tileMap.TileWidth)
+		return
+	}
 }
